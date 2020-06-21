a8f5cf5ae04ecee610aecb35b41bad080e80b299
wasanbi <59924700+wasanbi@users.noreply.github.com>
2020-06-21 16:55:40 +0800

    Update and rename jd_pet.js to jd_pet-x135qx.js

--- jd_pet-x135qx.js
+++ jd_pet-x135qx.js
@@ -7,8 +7,11 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 const cookie = $prefs.valueForKey('CookieJD')
 
 var shareCodes = [ // 这个列表填入你要助力的好友的shareCode, 最多可能是5个? 没有验证过
-    'MTAxODcxOTI2NTAwMDAwMDAwMDc4MDExNw==',
-    'MTAxODcxOTI2NTAwMDAwMDAyNjA4ODQyMQ==',
+    'MTAxODcxMjg4ODAwMDAwMDAwMDA5MzQ0Mw==',
+    'MTAxODcxOTI2NTAwMDAwMDAxNTYyNTQzMQ==',
+    'MTAxODc2NTEzMTAwMDAwMDAxMjI5NzU2NQ==',
+    'MTAxODc2NTEzMzAwMDAwMDAxMTgzODM4NQ==',
+    'MTAxODc2NTEzNDAwMDAwMDAwOTMwNDMwMw==',
 ]
 
 var petInfo = null;
